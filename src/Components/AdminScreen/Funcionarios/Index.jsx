3c1a74ade7441f6ad27548d423f20866e217a10c
unknown <gustavofrez86@gmail.com>
2021-08-31 01:19:27 -0300

    Contato responsivo finalizado

--- src/Components/AdminScreen/Funcionarios/Index.jsx
+++ src/Components/AdminScreen/Funcionarios/Index.jsx
@@ -1,121 +1,133 @@
 import React from 'react';
 import Styles from './Funcionarios.module.css';
-import lixeira from '../../../Assets/img/lixeira.png'
-import editar from '../../../Assets/img/ferramenta-lapis.png'
-import loader from '../../../Assets/img/loader.gif'
+import lixeira from '../../../Assets/img/lixeira.png';
+import editar from '../../../Assets/img/ferramenta-lapis.png';
+import loader from '../../../Assets/img/loader.gif';
 import { useState, useEffect } from 'react';
 import Modal from '../../ModalFunc/ModalFunc';
 import ModalConfirm from '../../ModalConfirm/ModalConfirm';
 import ModalUpdate from '../../ModalUpdateFunc/ModalUpdate';
 
 const Funcionarios = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isConfirmVisible, setIsConfirmVisible] = useState(false);
+  const [isUpdateVisible, setIsUpdateVisible] = useState(false);
 
-  const [isModalVisible, setIsModalVisible] = useState(false)
-  const [isConfirmVisible, setIsConfirmVisible] = useState(false)
-  const [isUpdateVisible, setIsUpdateVisible] = useState(false)
+  const [deleteId, setDeleteID] = useState(0);
+  const [updateId, setUpdateID] = useState(0);
+  const [func, setFunc] = useState([]);
 
-  const [deleteId, setDeleteID] = useState(0)
-  const [updateId, setUpdateID] = useState(0)
-  const [func, setFunc] = useState([])
-
-  useEffect(()=>{ 
-    
-    if(!func.length){
-      buscaFunc()
+  useEffect(() => {
+    if (!func.length) {
+      buscaFunc();
     }
-    console.log(func)
+    console.log(func);
+  });
 
-  })
+  const buscaFunc = async () => {
+    let request = await fetch(
+      'https://api-rest-funcionarios.herokuapp.com/funcionarios/',
+    );
+    let json = await request.json();
+    let funcionarios = json.result;
 
+    setFunc(funcionarios);
+  };
 
-  const buscaFunc = async ()=>{
-    let request = await fetch('https://api-rest-funcionarios.herokuapp.com/funcionarios/')
-    let json = await request.json()
-    let funcionarios = json.result
-    
-    setFunc(funcionarios)
+  const callModal = () => {
+    setIsModalVisible(!isModalVisible);
+  };
 
-  }
+  const callConfirm = (e) => {
+    setDeleteID(e.target.id);
+    setIsConfirmVisible(!isConfirmVisible);
+  };
 
-  const callModal = ()=>{
-    setIsModalVisible(!isModalVisible)
-  }
-
-  const callConfirm = (e)=>{
-    setDeleteID(e.target.id)
-    setIsConfirmVisible(!isConfirmVisible)
-  }
-
-  const callUpdate = (e)=>{
-    setUpdateID(e.target.id)
-    setIsUpdateVisible(!isConfirmVisible)
-  }
+  const callUpdate = (e) => {
+    setUpdateID(e.target.id);
+    setIsUpdateVisible(!isConfirmVisible);
+  };
 
   return (
     <div className={Styles.container}>
-      
-         <h1>Funcionários</h1>
-{ func.length ?       
-<table>
-  <thead>
-    <tr>
-      <th className={Styles.id}>ID</th>
-      <th>Nome</th>
-      <th className={Styles.idade}>Idade</th>
-      <th>CPF</th>
-      <th className={Styles.cargo}>Cargo</th>
-      <th className={Styles.editar}></th>
-    </tr>
-    </thead>
-  <tbody>
-  {func.map(fun =>(
-  <tr key={fun.ID}>      
-      
-    <td>{fun.ID}</td>
-    <td>{fun.NOME}</td>
-    <td>{fun.IDADE}</td>
-    <td>{fun.CPF}</td>
-    <td>{fun.CARGO}</td>      
+      <h1>Funcionários</h1>
+      {func.length ? (
+        <table>
+          <thead>
+            <tr>
+              <th className={Styles.id}>ID</th>
+              <th>Nome</th>
+              <th className={Styles.idade}>Idade</th>
+              <th>CPF</th>
+              <th className={Styles.cargo}>Cargo</th>
+              <th className={Styles.editar}></th>
+            </tr>
+          </thead>
+          <tbody>
+            {func.map((fun) => (
+              <tr key={fun.ID}>
+                <td>{fun.ID}</td>
+                <td>{fun.NOME}</td>
+                <td>{fun.IDADE}</td>
+                <td>{fun.CPF}</td>
+                <td>{fun.CARGO}</td>
 
-    <td><img className={Styles.img} id={fun.ID} onClick={callUpdate} src={editar} alt="Atualizar"/> <img className={Styles.img} id={fun.ID} onClick={callConfirm} src={lixeira} alt="Deletar"/></td>
+                <td>
+                  <img
+                    className={Styles.img}
+                    id={fun.ID}
+                    onClick={callUpdate}
+                    src={editar}
+                    alt="Atualizar"
+                  />{' '}
+                  <img
+                    className={Styles.img}
+                    id={fun.ID}
+                    onClick={callConfirm}
+                    src={lixeira}
+                    alt="Deletar"
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className={Styles.loader}>
+          <img src={loader} />
+        </div>
+      )}
 
-  </tr>      
+      {isModalVisible ? (
+        <Modal
+          apiUrl="https://api-rest-funcionarios.herokuapp.com/funcionarios/"
+          buscaFunc={() => buscaFunc()}
+          onClose={() => setIsModalVisible(false)}
+        />
+      ) : null}
 
-))}
-    
-  </tbody> 
+      {isConfirmVisible ? (
+        <ModalConfirm
+          apiUrl="https://api-rest-funcionarios.herokuapp.com/funcionarios/"
+          deleteId={deleteId}
+          busca={() => buscaFunc()}
+          onConfirm={() => setIsConfirmVisible(false)}
+        />
+      ) : null}
 
-</table> : <div className={Styles.loader}><img  src={loader}/></div>}
+      {isUpdateVisible ? (
+        <ModalUpdate
+          isUpdateVisible={isUpdateVisible}
+          apiUrl="https://api-rest-funcionarios.herokuapp.com/funcionarios/"
+          updateId={updateId}
+          buscaFunc={() => buscaFunc()}
+          onUpdate={() => setIsUpdateVisible(false)}
+        />
+      ) : null}
 
-
-
-{isModalVisible ? 
-  <Modal
-  apiUrl='https://api-rest-funcionarios.herokuapp.com/funcionarios/'   
-  buscaFunc={()=>buscaFunc()}
-  onClose={()=> setIsModalVisible(false)}
-  /> : null}
-
-{isConfirmVisible ?
-  <ModalConfirm
-  apiUrl='https://api-rest-funcionarios.herokuapp.com/funcionarios/'
-  deleteId={deleteId}
-  busca={()=>buscaFunc()}
-  onConfirm={()=>setIsConfirmVisible(false)}
-  /> :null}
-
-  {isUpdateVisible ?
-
-  <ModalUpdate
-  isUpdateVisible={isUpdateVisible}
-  apiUrl='https://api-rest-funcionarios.herokuapp.com/funcionarios/'
-  updateId={updateId}
-  buscaFunc={()=>buscaFunc()}
-  onUpdate={()=>setIsUpdateVisible(false)}
-  /> :null}
-
-
-    <button className={Styles.btnModal} onClick={()=> callModal()}>+</button>
+      <button className={Styles.btnModal} onClick={() => callModal()}>
+        +
+      </button>
     </div>
   );
 };
--- src/Components/Contato/Index2.jsx
+++ src/Components/Contato/Index2.jsx
@@ -2,152 +2,149 @@ import React, { useState } from 'react';
 import Styles from './Contato.module.css';
 import { Formik } from 'formik';
 
-
 const Contato2 = () => {
+  const [nome, setNome] = useState('');
 
-    const [nome, setNome] = useState('')
+  const onClick = () => {
+    console.log('SUBMIT');
+    console.log('Clicado');
+  };
 
-    const onClick = () => {
+  const handleNome = (e) => {
+    console.log(e.target.value);
+    setNome(e.target.value);
+  };
 
-        console.log('SUBMIT');
-        console.log("Clicado")
+  const enviaEmail = async (object) => {
+    const { nome, email, mensagem } = object;
 
-    }
+    const Email = {
+      texto: mensagem,
+      cliente: nome,
+      email: email,
+    };
 
-    const handleNome = (e) => {
-        console.log(e.target.value)
-        setNome(e.target.value)
-    }
+    let post = {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      }),
+      body: JSON.stringify(Email),
+    };
 
-    const enviaEmail = async (object) => {
+    let url = 'https://resiliamailservice.herokuapp.com/sendmail';
 
-        const { nome, email, mensagem } = object
+    const json = await fetch(url, post);
 
-        const Email = { 
-            texto: mensagem,
-            cliente: nome,
-            email: email 
-        }
+    console.log(json, Email);
+  };
 
-        let post = {
-            method: "POST",
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            }),
-            body: JSON.stringify(Email)
-        };
+  return (
+    <main className={Styles.pai}>
+      <section className={Styles.backgroundTopo}>
+        <h2>Contato</h2>
+      </section>
+      <div className={Styles.backgroundContato}>
+        <div className={Styles.contato}>
+          <div className={Styles.caixaContato}>
+            <h5 className={Styles.tituloContato}> Telefone</h5>
+            <p className={Styles.textoContato}>+55 (12) 3456-7890</p>
+          </div>
+          <div className={Styles.caixaContato}>
+            <h5 className={Styles.tituloContato}> Localização</h5>
+            <p className={Styles.textoContato}>
+              R.Hans Staden, 20 - Botafogo, Rio de Janeiro - RJ
+            </p>
+          </div>
+          <div className={Styles.caixaContato}>
+            <h5 className={Styles.tituloContato}> Endereço de E-mail</h5>
+            <p className={Styles.textoContato}>contact@solari.com</p>
+          </div>
+        </div>
 
-        let url = 'https://resiliamailservice.herokuapp.com/sendmail'
+        <Formik
+          initialValues={{ nome: '', email: '', mensagem: '' }}
+          validate={(values) => {
+            values.nome = nome;
+            const errors = {};
+            if (!values.nome) {
+              errors.nome = 'Required';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'Email invalido';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              console.log(JSON.stringify(values, null, 2));
+              enviaEmail(values);
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+          }) => (
+            <form onSubmit={handleSubmit} className={Styles.formContato}>
+              <h5 className={Styles.tituloFormulario}>
+                Nos envie uma mensagem
+              </h5>
+              <input
+                className={Styles.inputContato}
+                type="text"
+                name="Nome"
+                onChange={handleNome}
+                onBlur={handleBlur}
+                value={nome}
+                placeholder="Nome"
+              />
+              {errors.nome && touched.nome && errors.nome}
 
-       const json = await fetch(url, post)
+              <input
+                className={Styles.inputContato}
+                type="email"
+                name="email"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+                placeholder="E-mail"
+              />
+              {errors.email && touched.email && errors.email}
 
-        console.log(json, Email)
-
-    }
-
-
-
-    return (
-
-        <main>
-            <section className={Styles.backgroundTopo}>
-                <h2>Contato</h2>
-            </section>
-            <div className={Styles.backgroundContato}>
-                <div className={Styles.contato}>
-                    <div>
-                        <h2 className={Styles.titulo}>Telefone</h2>
-                        <h2>+55 (12) 3456-7890</h2>
-                    </div>
-                    <div>
-                        <h2 className={Styles.titulo}>Localização</h2>
-                        <h2>R.Hans Staden, 20 - Botafogo, Rio de Janeiro - RJ</h2>
-                    </div>
-                    <div>
-                        <h2 className={Styles.titulo}>Endereço de E-mail</h2>
-                        <h2>contact @solari.com</h2>
-                    </div>
-                </div>
-                <div className={Styles.endereco}></div>
-            </div>
-            <Formik
-                initialValues={{ nome: '', email: '', mensagem: '' }}
-                validate={(values) => {
-                    values.nome = nome
-                    const errors = { }
-                    if (!values.nome) {
-                        errors.nome = 'Required'
-                    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-                        errors.email = 'Email invalido';
-                    }
-                    return errors
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        console.log(JSON.stringify(values, null, 2));
-                        enviaEmail(values)   
-                        setSubmitting(false);
-                    }, 400);
-                }}
-            >
-
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting
-
-                }) => (
-                    <form onSubmit={handleSubmit} className={Styles.formContato}>
-                        <spam className={Styles.msgContato}>
-                            Nos envie uma mensagem!
-                        </spam>
-                        <label> Nome: </label>
-                        <input
-                            className={Styles.inputContato}
-                            type="text"
-                            name="Nome"
-                            onChange={handleNome}
-                            onBlur={handleBlur}
-                            value={nome}
-                        />
-                        {errors.nome && touched.nome && errors.nome}
-                        <label> E-mail:</label>
-                        <input
-                            className={Styles.inputContato}
-                            type="email"
-                            name="email"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.email}
-
-                        />
-                        {errors.email && touched.email && errors.email}
-                        <label>Mensagem:</label>
-                        <input
-                            className={Styles.inputContato}
-                            type="text"
-                            name="mensagem"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.mensagem}
-                            required
-                        />
-                        {errors.mensagem && touched.mensagem && errors.mensagem}
-                        <button type="submit" onClick={onClick} disabled={isSubmitting}>
-                            Enviar
-                        </button>
-                    </form>
-                )}
-            </Formik>
-        </main >
-
-    );
-
+              <input
+                className={Styles.inputContato}
+                type="text"
+                name="mensagem"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.mensagem}
+                required
+                placeholder="Mensagem"
+              />
+              {errors.mensagem && touched.mensagem && errors.mensagem}
+              <button
+                className={Styles.botaoContato}
+                type="submit"
+                onClick={onClick}
+                disabled={isSubmitting}
+              >
+                Enviar
+              </button>
+            </form>
+          )}
+        </Formik>
+      </div>
+    </main>
+  );
 };
 
-export default Contato2
+export default Contato2;
